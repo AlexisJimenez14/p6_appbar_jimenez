8251b6d35bcfb998811f0802f3c3e4eadb41ada6
ALEXIS_FABIAN_JIMENEZ_JIMENEZ <a.22308051281213@cbtis128.edu.mx>
2025-02-19 20:33:46 +0000

    enviar practica 6

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
       title: 'Flutter Hello World',
       // Application theme data, you can set the colors for the application as
@@ -17,25 +18,40 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Jimenez AppBar'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {}, //lo que esta entre llaves son las acciones
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.more_vert,
+            ),
+            onPressed: () {},
+          )
+        ],
+        backgroundColor: Color(0xff7ca5ea),
         // The title text which will be shown on the action bar
         title: Text(title),
+        leading: const Icon(Icons.arrow_back),
+        centerTitle: true,
       ),
-      body: Center(
+      body: const Center(
         child: Text(
-          'Hello, World!',
+          'Hola Alexis Fabina Jimenez Jimenez con AppBar',
         ),
       ),
     );
